8f535ade99b5ef1fe921ffab34a020282f32fa11
minwooji <jimw0201@gmail.com>
2024-10-08 10:32:14 +0900

    소년 객체 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -11,7 +11,19 @@ class Grass:
         self.image.draw(400, 30)
 
     def update(self): pass
-    pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 def handle_events():
     global running
